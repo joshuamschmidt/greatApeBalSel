dbd2833e5b2b45c55033158121574319c06580b3
joshuamschmidt <joshmschmidt1@gmail.com>
2021-03-29 18:15:15 +1030

    single species results gene set tests

--- gene_set_tests/pysetperm.py
+++ gene_set_tests/pysetperm.py
@@ -110,10 +110,10 @@ def make_results_table(test_obj, function_set_obj, set_perm_obj):
     out['mean_n_resample'] = set_perm_obj.mean_per_set
     out['emp_p_e'] = set_perm_obj.p_enrichment
     out['emp_p_d'] = set_perm_obj.p_depletion
-    out['fdr_e'] = psp.fdr_from_p_matrix(set_perm_obj.set_n_per_perm, out['emp_p_e'], method='enrichment')
-    out['fdr_d'] = psp.fdr_from_p_matrix(set_perm_obj.set_n_per_perm, out['emp_p_d'], method='depletion')
-    out['BH_fdr_e'] = psp.p_adjust_bh(out['emp_p_e'])
-    out['BH_fdr_d'] = psp.p_adjust_bh(out['emp_p_d'])
+    out['fdr_e'] = fdr_from_p_matrix(set_perm_obj.set_n_per_perm, out['emp_p_e'], method='enrichment')
+    out['fdr_d'] = fdr_from_p_matrix(set_perm_obj.set_n_per_perm, out['emp_p_d'], method='depletion')
+    out['BH_fdr_e'] = p_adjust_bh(out['emp_p_e'])
+    out['BH_fdr_d'] = p_adjust_bh(out['emp_p_d'])
     out = out.sort_values('emp_p_e')
     return out
 
--- gene_set_tests/single_species_go.py
+++ gene_set_tests/single_species_go.py
@@ -1,24 +1,61 @@
 import pysetperm as psp
 import numpy as np
 import pandas as pd
+import pickle
 
-n_perms = 50000
+n_perms = 100000
 cores = 4
+outpath='/Users/joshuaschmidt/Projects/greatApeBalSel/gene_set_tests/'
 annotations = psp.AnnotationSet(annotation_file='../gene_annotations/hg19_nonRedundant_GOWINDA_noORS.bed', range_modification=2000)
-function_sets = psp.FunctionSets(function_set_file='../gene_sets/kegg_long.txt', min_set_size=10, annotation_obj=annotations)
-# specific inputs
-# eastern chimps
-e_candidates = psp.Variants(variant_file='../gene_set_test_files/schweinfurthii_candidate_windows.txt')
-e_candidates.annotate_variants(annotation_obj=annotations)
-e_background = psp.Variants(variant_file='../gene_set_test_files/schweinfurthii_background_windows.txt')
-e_background.annotate_variants(annotation_obj=annotations)
-e_test_obj = psp.TestObject(e_candidates,
-                            e_background,
-                            function_sets,
-                            n_cores=cores)
-e_permutations = psp.Permutation(e_test_obj, n_perms, cores)
-e_per_set = psp.SetPerPerm(e_permutations,
-                           function_sets,
-                           e_test_obj,
-                           cores)
-e_results = make_results_table(e_test_obj, function_sets, e_per_set)
+tests = ['vips', 'kegg']
+for test_set in tests:
+    function_sets = psp.FunctionSets(function_set_file='../gene_sets/'+test_set+'_long_reformat.txt', min_set_size=5, annotation_obj=annotations)
+    species = ['abelii', 'pygmaeus', 'gorilla', 'graueri', 'bonobo', 'troglodytes', 'schweinfurthii', 'ellioti', 'verus']
+    for sp in species:
+        candidates = psp.Variants(variant_file='../gene_set_test_files/'+sp+'_candidate_windows.txt')
+        candidates.annotate_variants(annotation_obj=annotations)
+        background = psp.Variants(variant_file='../gene_set_test_files/'+sp+'_background_windows.txt')
+        background.annotate_variants(annotation_obj=annotations)
+        test_obj = psp.TestObject(candidates,
+                                  background,
+                                  function_sets,
+                                  n_cores=cores)
+        permutations = psp.Permutation(test_obj, n_perms, cores)
+        per_set = psp.SetPerPerm(permutations,
+                                 function_sets,
+                                 test_obj,
+                                 cores)
+        results = psp.make_results_table(test_obj, function_sets, per_set)
+        results.to_csv(path_or_buf=outpath+sp+'_'+test_set+'_set.test.txt', sep='\t')
+
+
+#with open('filename.pickle', 'wb') as handle:
+#    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+# add species....
+n_perms = 50000
+cores = 6
+all_test_obj = []
+all_permutations = []
+all_per_set = []
+for test_set in tests:
+    function_sets = psp.FunctionSets(function_set_file='../gene_sets/'+test_set+'_long_reformat.txt', min_set_size=5, annotation_obj=annotations)
+    species = ['abelii', 'pygmaeus', 'gorilla', 'graueri', 'bonobo', 'troglodytes', 'schweinfurthii', 'ellioti', 'verus']
+    for sp in species:
+        candidates = psp.Variants(variant_file='../gene_set_test_files/'+sp+'_candidate_windows.txt')
+        candidates.annotate_variants(annotation_obj=annotations)
+        background = psp.Variants(variant_file='../gene_set_test_files/'+sp+'_background_windows.txt')
+        background.annotate_variants(annotation_obj=annotations)
+        test_obj = psp.TestObject(candidates,
+                                  background,
+                                  function_sets,
+                                  n_cores=cores)
+        all_test_obj.append(test_obj)
+        permutations = psp.Permutation(test_obj, n_perms, cores)
+        all_permutations.append(permutations)
+        per_set = psp.SetPerPerm(permutations,
+                                 function_sets,
+                                 test_obj,
+                                 cores)
+        all_per_set.append(per_set)
